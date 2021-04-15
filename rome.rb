49c5e9095d1d4adf9f76b68f3be152c4b90ae799
Evan Coleman <e@edc.me>
2021-04-15 17:32:46 -0400

    fix formula

--- rome.rb
+++ rome.rb
@@ -1,12 +1,13 @@
 class Rome < Formula
   desc "A shared cache tool for Carthage"
   homepage "https://github.com/tkww/Rome"
-  url "https://github.com/blender/Rome/releases/download/v0.23.3.64/rome.zip"
+  url "https://github.com/blender/Rome/archive/v0.23.3.64.zip"
   sha256 "a7bf815a3491119a038f70a975f1d3331a519edb09f4e0aaa7eea68e4b8e9f39"
 
   bottle :unneeded
 
   def install
+    system "stack build"
     bin.install "rome"
   end
 
