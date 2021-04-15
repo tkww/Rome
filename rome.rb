c9c66549fc1f6c280736ce68003df51acb2ebd16
Evan Coleman <e@edc.me>
2021-04-15 17:25:43 -0400

    add brew formula

---
+++ rome.rb
@@ -0,0 +1,16 @@
+class Rome < Formula
+  desc "A shared cache tool for Carthage"
+  homepage "https://github.com/tkww/Rome"
+  url "https://github.com/blender/Rome/releases/download/v0.23.3.64/rome.zip"
+  sha256 "a7bf815a3491119a038f70a975f1d3331a519edb09f4e0aaa7eea68e4b8e9f39"
+
+  bottle :unneeded
+
+  def install
+    bin.install "rome"
+  end
+
+  test do
+    system "#{bin}/rome", "--version"
+  end
+end
